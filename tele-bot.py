32527300082975c63b74521c545cc9b629e60ce9
soxgsvzztafah <113221719+soxgsvzztafah@users.noreply.github.com>
2023-07-10 21:26:27 +0530

    Update tele-bot.py

--- tele-bot.py
+++ tele-bot.py
@@ -1,7 +1,7 @@
 import os
 import telebot
 
-bot = telebot.TeleBot("API Key Here")
+bot = telebot.TeleBot("6086029944:AAEZfp7PbmFahcCwb7-qelmZAsb56jaAT7I")
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
